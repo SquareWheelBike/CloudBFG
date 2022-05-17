311c1c58611e235e4b7a2ba1ec7571879c8f08bb
SquareWheelBike <colefuerth@gmail.com>
2022-05-17 16:11:09 -0400

    began battsim development

---
+++ BattSim/octave/BattSim.py
@@ -0,0 +1,7 @@
+# Python script to run `octave demoBatterySimulator.m`, and provide an interface between Python and Octave for battery simulation.
+
+# todo: add HTML responses? Perhaps we can use a local server to simulate batteries, so the pi doesn't have to simulate the battery
+
+from os import subprocess
+
+sp = subprocess(STDIN=stdin, STDOUT=stdout)
