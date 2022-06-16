537c0331394dfd441b21bc39d1dd5707e72b9d86
Cole Fuerth <fuert11g@uwindsor.ca>
2022-06-16 17:15:21 -0400

    generated OCV curves

---
+++ src/showCurves.py
@@ -0,0 +1,18 @@
+# A demo file to graph the OCV curves, overlayed on each other, for all batteries in the K_para.csv file.
+
+import zsoc
+import matplotlib.pyplot as plt
+
+INPUTFILE = '../res/K_para.csv'
+OUTPUTFOLDER = '../res/zsoc_curves'
+DECIMALS = 4
+batteries = zsoc.generate_curves(INPUTFILE, decimals=4, verbose=False, generate_csv=False)[1:]
+
+for battery in batteries:
+    plt.plot(battery[-2], battery[-1], label=battery[0])
+plt.title(label='OCV curves for all batteries')
+plt.xlabel(xlabel='zsoc')
+plt.ylabel(ylabel='Vo')
+plt.grid(True)
+# plt.legend()
+plt.show()
