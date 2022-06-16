4b06e83d47e801782cdbe0753c1fefe012fb13c9
Cole Fuerth <fuert11g@uwindsor.ca>
2022-06-16 14:25:18 -0400

    import k parameters

--- src/main.py
+++ src/main.py
@@ -1,4 +1,9 @@
-from BattSim.BattSim import BattSim as Battery
+from BattSim.BattSim import BattSim as bs
 import BattSim.CurrentSIM as cs
 import csv
 
+# import ../res/K_para.csv into a list of dictionary values
+with open('../res/K_para.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    K_para = list(reader)
+print(K_para)
