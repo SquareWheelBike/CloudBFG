bca45d3bd83d207e61b84febc8be103b1e6a84cd
Cole Fuerth <colefuerth@gmail.com>
2022-07-15 11:28:53 -0400

    curve lookups working (ideal)

--- gen1.py
+++ gen1.py
@@ -16,7 +16,8 @@ import numpy as np
 
 # generate the curves for each of the sample k parameters using zsoc.py
 INPUTFILE = 'res/K_para.csv'
-batteries = zsoc.generate_curves(INPUTFILE, verbose=False, generate_csv=False, resolution=200)
+batteries = zsoc.generate_curves(
+    INPUTFILE, verbose=False, generate_csv=False, resolution=200)
 
 # pick a random battery and create a battery object for it
 # Kbatt: list, Cbatt: float, R0: float, R1: float, C1: float, R2: float, C2: float, ModelID:int, soc:float=0.5
@@ -24,15 +25,15 @@ target_battery = batteries[np.random.randint(0, len(batteries))]
 # print(target_battery['k'])
 sim_battery = BattSim(
     Kbatt=target_battery['k'],
-    Cbatt = 2,
-    R0 = target_battery['R0'],
-    R1 = 0.1,
-    C1 = 5,
-    R2 = 0.3,
-    C2 = 500,
+    Cbatt=2,
+    R0=target_battery['R0'],
+    R1=0.1,
+    C1=5,
+    R2=0.3,
+    C2=500,
     soc=1.0,
     ModelID=1,
-) # note that only the Kbatt and soc is used for the simulation, the rest of the parameters are dummy values
+)  # note that only the Kbatt and soc is used for the simulation, the rest of the parameters are dummy values
 
 # simulate full discharge curve for the battery
 # discharge at 1C for 1h
@@ -46,7 +47,9 @@ Vbatt, Ibatt, soc, Vo = sim_battery.simulate(I, T)
 # essentially, the point of gen 1 is to just show that in ideal case, we can actually use a cache and recorded datapoints to lookup K values
 
 # find the rate of change of a vector passed
-def derivative(L, dt):
+
+
+def derivative(L: np.ndarray, dt: float = 1) -> np.ndarray:
     """
     find the rate of change of a vector
     L: numpy array, volts, dt: float, seconds between datapoints
@@ -60,22 +63,43 @@ def derivative(L, dt):
     return (L[1:] - L[:-1]) / dt
 
 # not even bothering with time vector at this stage
-def find_curve(V, batteries):
+
+
+def find_curve(V: np.ndarray, batteries: list[dict]):
     """
-    find the curve closest to the voltage and time data
-    V: numpy array of voltages, batteries: list of dicts of batteries
+    find the curve that fits closest to the Vo of one of the batteries
+    V: numpy array, volts
+    batteries: list of dictionaries, battery cache from zsoc.py
 
     returns the Kbatt of the battery that matches the data
-    if no match is found, returns None
     """
-    dV = derivative(V, 3600/200)
-    diffs = np.array([
-        # np.sum(np.abs(V - batt['Vo'])) for batt in batteries
-        np.sum(np.abs(dV - derivative(batt['Vo'], 3600/200))) for batt in batteries
-    ])
-    # find k parameters of the battery that matches the voltage data
-    # if no match is found, return None
-    return batteries[np.argmin(diffs)]['k']
+
+    # shuffle batteries
+    batteries = batteries[:]
+    np.random.shuffle(batteries)
+
+    delta = 3600 / 200
+
+    # find the rate of change of the Vo curve
+    # reverse V so slope is positive
+    V = V[::-1]
+    # find the rate of change of the Vo curve
+    # dV = derivative(V, delta)
+    # dV2 = derivative(dV, delta)
+
+    # this will NOT WORK if the datapoints being used are not numpy arrays
+    assert(type(V) is np.ndarray)
+    assert(all(type(battery['Vo']) is np.ndarray for battery in batteries))
+
+    # find the closest match to the sample curve
+    error = np.array(
+        [np.sum(np.abs(V - battery['Vo']))
+         for battery in batteries]
+    )
+
+    # return the battery that matches the closest match
+    return batteries[np.argmin(error)]
+
 
 print('expected Kbatt:\t', target_battery['k'])
-print('actual Kbatt:\t', find_curve(Vo, batteries))
+print('actual Kbatt:\t', find_curve(Vo, batteries)['k'])
