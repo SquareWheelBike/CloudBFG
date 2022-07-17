a7aed345e69d1bf8b9ff1801364e30d33abdc41f
Cole Fuerth <colefuerth@gmail.com>
2022-07-16 21:05:17 -0400

    determine straightness of a line

--- src/tools.py
+++ src/tools.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def derivative(L: np.ndarray, dt: float = 1) -> np.ndarray:
     """
     find the rate of change of a vector
@@ -13,12 +14,41 @@ def derivative(L: np.ndarray, dt: float = 1) -> np.ndarray:
         L = np.array(L)
     return (L[1:] - L[:-1]) / dt
 
-def integrate_subtract(a: np.ndarray, b: np.ndarray) -> np.ndarray:
+
+def integrate_subtract(a: np.ndarray, b: np.ndarray) -> float:
     """
-    integrate the difference of two vectors
+    integrate the absolute value difference of two one-dimensional vectors
     a: numpy array, volts
     b: numpy array, volts
 
     returns the integrated result
     """
     return np.sum(np.abs(a - b))
+
+
+def how_straight(a: np.ndarray) -> float:
+    """
+    metric for how straight a vector of points is
+
+    a: numpy array (2d), integers
+
+    returns int, lower is straighter
+    """
+
+    return integrate_subtract(a, np.ones(len(a)) * np.mean(a))
+
+# dummy RC values for testing
+dummy_RC = [
+    0.1,  # R1
+    5,    # C1
+    0.3,  # R2
+    500,  # C2
+]
+
+def soc_curve_k(k_param:list, resolution=200) -> tuple[np.ndarray]:
+    sim = BattSim(guess_k, 1, *dummy_RC, soc=1.0, ModelID=1)
+    delta = 3600 / resolution  # using 200 points on everything
+    I = np.ones(resolution) * sim.Cbatt * -1
+    T = np.arange(0, 3600, delta)
+    Vbatt, Ibatt, soc, Vo = sim.simulate(I, T, sigma_v=0)
+    return Vo, soc
